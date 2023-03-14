a1b29b8a5eacac2086bbe7c8accea642a092d021
Shoaibk242 <100289342+Shoaibk242@users.noreply.github.com>
2023-03-14 19:10:31 +0530

    Update and rename testb.py to DNA_GC_content.py

--- DNA_GC_content.py
+++ DNA_GC_content.py
@@ -10,15 +10,3 @@ print("the gc content is ",gc_content(myseq))
 from Bio.Seq import Seq
 from Bio.SeqUtils import GC
 print (GC(myseq))
-#recursive function factorial
-n=input("Enter a non negative number")
-n=int(n)
-def factorial(x):
-    if x < 1 :
-        return 1
-    else:
-        return (x*factorial(x-1))
-print("the factorial of the given number is ",factorial(n))'''
-import calc
-x=calc.add(6,5)
-print(x)
